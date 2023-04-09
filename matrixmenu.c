ba26d70de51b86fc352b612e79bf99c1f885c5d6
Abhijeet <abhikmr1899@gmail.com>
2023-04-09 23:26:14 +0530

    updated

--- matrixmenu.c
+++ matrixmenu.c
@@ -7,121 +7,92 @@
 
 #include <stdio.h>
 
-int main()
-{
-    int array[100], choice, position, value, n, i;
-    printf("ABHIJEET KUMAR MCA 2nd SEM 42\n");
-    printf("ARRAY MENU DRIVEN PROGRAM....\n");
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
-
-    // loop to input elements in the array
-    for (i = 0; i < n; i++)
-    {
-        printf("Enter element %d: ", i+1);
-        scanf("%d", &array[i]);
-    }
-
-    while (1)
-    {
-        printf("\nMenu\n");
-        printf("1. Traverse the array\n");
-        printf("2. Insert an element at the last position\n");
-        printf("3. Insert an element at a specific position\n");
-        printf("4. Delete the last element\n");
-        printf("5. Delete an element from a specific position\n");
-        printf("6. Search for an element\n");
-        printf("7. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch(choice)
-        {
-            case 1:
-                printf("Elements in the array: ");
-                for (i = 0; i < n; i++)
-                {
-                    printf("%d ", array[i]);
-                }
-                break;
-
-            case 2:
-                printf("Enter the element to insert at last position: ");
-                scanf("%d", &value);
-                array[n] = value;
-                n++;
-                printf("Element inserted successfully!\n");
-                break;
-
-            case 3:
-                printf("Enter the position where you want to insert the element: ");
-                scanf("%d", &position);
-                printf("Enter the element to insert: ");
-                scanf("%d", &value);
-                for (i = n-1; i >= position-1; i--)
-                {
-                    array[i+1] = array[i];
-                }
-                array[position-1] = value;
-                n++;
-                printf("Element inserted successfully!\n");
-                break;
-
-            case 4:
-                if (n == 0)
-                {
-                    printf("Array is empty!\n");
-                }
-                else
-                {
-                    n--;
-                    printf("Last element deleted successfully!\n");
-                }
-                break;
-
-            case 5:
-                if (n == 0)
-                {
-                    printf("Array is empty!\n");
-                }
-                else
-                {
-                    printf("Enter the position of the element to delete: ");
-                    scanf("%d", &position);
-                    for (i = position-1; i < n-1; i++)
-                    {
-                        array[i] = array[i+1];
-                    }
-                    n--;
-                    printf("Element deleted successfully!\n");
-                }
-                break;
-
-            case 6:
-                printf("Enter the element to search: ");
-                scanf("%d", &value);
-                for (i = 0; i < n; i++)
-                {
-                    if (array[i] == value)
-                    {
-                        printf("Element found at position %d\n", i+1);
-                        break;
-                    }
-                }
-                if (i == n)
-                {
-                    printf("Element not found!\n");
-                }
-                break;
-
-            case 7:
-                printf("Exiting the program...\n");
-                return 0;
-
-            default:
-                printf("Invalid choice! Please try again.\n");
+int main() {
+    int choice, i, j, m, n;
+    int mat[10][10], res[10][10];
+    printf("Enter the number of rows and columns of the matrix: ");
+    scanf("%d %d", &m, &n);
+    printf("Enter the elements of the matrix:\n");
+    for(i=0; i<m; i++) {
+        for(j=0; j<n; j++) {
+            scanf("%d", &mat[i][j]);
         }
     }
-
-    return 0;
+    printf("Choose the matrix operation:\n");
+    printf("1. Multiplication\n");
+    printf("2. Transpose\n");
+    printf("3. Diagonal Swap\n");
+    scanf("%d", &choice);
+    switch(choice) {
+        case 1:
+            printf("Enter the elements of the second matrix:\n");
+            int mat2[10][10];
+            int k, l, p;
+            printf("Enter the number of rows and columns of the second matrix: ");
+            scanf("%d %d", &k, &l);
+            if(n!=k) {
+                printf("Error: The number of columns of the first matrix should be equal to the number of rows of the second matrix.\n");
+                return 0;
+            }
+            printf("Enter the elements of the second matrix:\n");
+            for(i=0; i<k; i++) {
+                for(j=0; j<l; j++) {
+                    scanf("%d", &mat2[i][j]);
+                }
+            }
+            for(i=0; i<m; i++) {
+                for(j=0; j<l; j++) {
+                    res[i][j] = 0;
+                    for(p=0; p<n; p++) {
+                        res[i][j] += mat[i][p] * mat2[p][j];
+                    }
+                }
+            }
+            printf("The result of matrix multiplication:\n");
+            for(i=0; i<m; i++) {
+                for(j=0; j<l; j++) {
+                    printf("%d ", res[i][j]);
+                }
+                printf("\n");
+            }
+            break;
+        case 2:
+            printf("The transpose of the matrix:\n");
+            for(i=0; i<n; i++) {
+                for(j=0; j<m; j++) {
+                    printf("%d ", mat[j][i]);
+                }
+                printf("\n");
+            }
+            break;
+        case 3:
+            if(m!=n) {
+                printf("Error: The matrix should be square for diagonal swap.\n");
+                return 0;
+            }
+            printf("The original matrix:\n");
+            for(i=0; i<m; i++) {
+                for(j=0; j<n; j++) {
+                    printf("%d ", mat[i][j]);
+                }
+                printf("\n");
+            }
+            for(i=0; i<m; i++) {
+                int temp = mat[i][i];
+                mat[i][i] = mat[i][n-i-1];
+                mat[i][n-i-1] = temp;
+            }
+            printf("The matrix after diagonal swap:\n");
+            for(i=0; i<m; i++) {
+                for(j=0; j<n; j++) {
+                    printf("%d ", mat[i][j]);
+                }
+                printf("\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid choice.\n");
+            break;
+    }
+return 0;
 }
